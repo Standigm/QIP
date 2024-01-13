c3af6a855e52a2418fcee3eab0e867125e4f539c
InSuk Joung <insuk.joung@standigm.com>
2024-01-13 18:52:19 +0900

    maint: install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+conda create -n qip python=3.10
+mamba install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+mamba install -c pyg pyg
+mamba install -c conda-forge lightning python-dotenv hydra-core hydra-colorlog rdkit rich
+mamba install -c openeye openeye-toolkits
+mamba install -c pyg pytorch-scatter
+pip install -e .
