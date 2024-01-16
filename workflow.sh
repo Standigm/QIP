2c2d118cb7e5b7cacdc407a2b87970fd65d2852f
jungwookim <jungwoo.kim@standigm.com>
2024-01-16 23:02:10 +0900

    fix finetuning code

--- workflow.sh
+++ workflow.sh
@@ -3,14 +3,19 @@ python preprocessing.py
 
 seed_num=8272
 # run multi task learning
-python run.py experiment=encoder_train/gps/multitask/gps_MTHAD.yaml seed=${seed_num} 
+python run.py experiment=encoder_train/gps/multitask/gps_MTHAD.yaml seed=${seed_num} # trainer.max_steps=10 datamodule.batch_size=10
 
  # run finetuning
 root='./configs/experiment/encoder_train/gps/finetuning/template/*'
 for d in $root; 
 do
-    echo $d
-    python run.py experiment=$d seed=${seed_num}
+    
+    replace='./configs/experiment/'
+    replacewith=''
+    path="${d/${replace}/${replacewith}}"
+    echo $path
+    
+    python run.py experiment=$path seed=${seed_num} # trainer.max_steps=10 datamodule.batch_size=10
 done
 
 # inference test
